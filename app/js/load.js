2e0fb07c4c61c73b5ee4890f2483c0c783cf86eb
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2021-08-23 20:48:34 +0400

    pls work.html

--- app/js/load.js
+++ app/js/load.js
@@ -5,7 +5,7 @@ fetch(`https://ge-employees.herokuapp.com/data/${subject}`)
   .then((papers) => {
     papers.forEach((element) => {
       $(".papers").append(
-        `-><a href = ${element.link}>${element.subject} ${element.exam} Set ${element.set} (${element.type})</a><br>`
+        `-><a href = ${element.link} target="page">${element.subject} ${element.exam} Set ${element.set} (${element.type})</a><br>`
       );
     });
   });
