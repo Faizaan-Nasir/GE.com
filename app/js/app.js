7921fb4521dc6d76f07f125b37f961a6789d5f6a
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2021-08-28 21:19:01 +0400

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -4,99 +4,99 @@ function check() {
   let b = document.getElementById("password").value;
   if (a == "Aahil" && b == "s8626") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Aamir" && b == "s8872") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Aashray" && b == "s12901") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Adwaith" && b == "s10832") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Ashar" && b == "okpass@21") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Ashaz" && b == "s16166") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Aneesh" && b == "s8376") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "S8395" && b == "22arnav2005") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Atharv" && b == "athu2006") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Ayush" && b == "s8670") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Daksh" && b == "s8342") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Faizaan" && b == "s8349") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Hussain" && b == "s10717") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Shaasvat" && b == "s15280") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Krish" && b == "s8806") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Maaz" && b == "s9000") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Mohammad" && b == "s8753") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Mushaf" && b == "s9012") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Maahir" && b == "s13521") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Raza" && b == "s9128") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Ahemeduddin" && b == "s10120") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Nadeem" && b == "s8879") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Rishit" && b == "s15749") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Sabhya" && b == "s8340") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Sath-Hya" && b == "GEFounderS14234") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Satvik" && b == "s8973") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "amazinglySK" && b == "anonymousGuiseppe2302") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Shiv" && b == "s15888") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Tanmeen" && b == "s14218") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Veer" && b == "s8985") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Harsha" && b == "s12915") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else if (a == "Random" && b == "People") {
     localStorage.name = a;
-    window.location.href = "/home";
+    window.location.href = "./home.html";
   } else alert("Invalid Login ID and Password");
 }
