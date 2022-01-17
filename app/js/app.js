ee5e6d9ab611d7c9166d78f8b729452e99c4f1fd
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2022-01-17 14:06:30 +0400

    THE CODE ACTUALLY WORKS

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,38 @@
+function check1() {
+  // Selecting the input element and get its value
+  let a = document.getElementById("inputId1").value;
+  let b = document.getElementById("password1").value;
+
+  const jsonToPost = {
+    usr_name: a,
+    pwd: b,
+  };
+
+  fetch("https://ge-employees.herokuapp.com/authenticate?type=normal", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jsonToPost),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      if (json.message == "Authenticated") {
+        localStorage.name = a;
+        window.location.href = "./home.html";
+      } else {
+        alert("Invalid Login ID and Password");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      alert(
+        "Oops. there seems to be some error with our systems. Please contact the devs"
+      );
+    });
+}
 function check() {
   // Selecting the input element and get its value
   let a = document.getElementById("inputId").value;
