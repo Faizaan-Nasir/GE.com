2efec055ae58b29044e7614c72ca79bf043c7eea
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2022-01-17 16:14:07 +0400

    Made multiple backgrounds

---
+++ app/js/js.js
@@ -0,0 +1,18 @@
+function choose() {
+    let x = Math.floor((Math.random() * 5) + 1);
+    if (x == 1) {
+        window.location.href = "./login1.html"
+    }
+    else if (x == 2) {
+        window.location.href = "./login2.html"
+    }
+    else if (x == 3) {
+        window.location.href = "./login3.html"
+    }
+    else if (x == 4) {
+        window.location.href = "./login4.html"
+    }
+    else if (x == 5) {
+        window.location.href = "./login5.html"
+    }
+}
